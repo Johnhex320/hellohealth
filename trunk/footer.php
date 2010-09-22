1470e3b1af0998ff2d2d2282f378dd3f54a70a92
sonpham007 <sonpham007@dbd9b7fb-4151-0cbc-ec3f-2951bdfe8026>
2010-09-22 09:36:57 +0000

    first upload in source control

--- trunk/footer.php
+++ trunk/footer.php
@@ -49,5 +49,9 @@
 
 	wp_footer();
 ?>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/media/js/cufon-yui.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/media/js/din_400.font.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/media/js/plugins.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/media/js/base.js"></script>
 </body>
 </html>
